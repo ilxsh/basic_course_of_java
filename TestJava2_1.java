00077eb4e0fc9370289172bfa82e228898e8625f
David <David@jdsu.com>
2016-12-12 23:43:46 +0800

    Initial update source code to repository.

---
+++ TestJava2_1.java
@@ -0,0 +1,11 @@
+public class TestJava2_1
+{
+    public static void main(String args[])
+    {
+        int num;    // Declare a integer variable num
+        num = 3;    // Assign 3 to num
+        // Output String, using '+' to concate
+        System.out.println("This is number "+num);
+        System.out.println("I have "+num+" books!");
+    }
+}
